a25399bef09a413910afcc74e7ded9f4c2613f08
Arpi65 <arpi.bandikyan@gmail.com>
2020-10-20 22:03:21 -0700

    Hoemwork version 1

--- 06-Python-APIs/Instructions/starter_code/config.py
+++ 06-Python-APIs/Instructions/starter_code/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "bbebc62a523ba84e046f71f16dac80e4"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyAmsu-ZLhxRamsJLcUIuZQ3hmpshPiyO70"
+g_key = ""
